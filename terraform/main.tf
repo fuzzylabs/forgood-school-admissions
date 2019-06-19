4b6c0e545ee10c33a97ab6d5a92d564f6e01f81d
Tom Stockton <tom@stocktons.org.uk>
2019-06-19 17:16:25 +0100

    Add local provisioner

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,31 +12,14 @@ resource "google_bigquery_table" "default" {
   description = "${lookup(var.tables[count.index], "description")}"
   schema      = "${file(lookup(var.tables[count.index], "schema"))}"
 
-    provisioner "local-exec" {
-      command = 'bq --location=${var.location} load --skip_leading_rows=1 --source_format=CSV ${var.project}:${google_bigquery_dataset.default.dataset_id}.${lookup(var.tables[count.index], "table_id") ${lookup(var.tables[count.index], "data")}'
-    }
+  # Loading data into the table isn't native to Terraform right now
+  # working around with local-exec to call out to bq
+  # https://github.com/terraform-providers/terraform-provider-google/issues/3868
+  provisioner "local-exec" {
+    command = <<EOT
+      bq --location=${var.location} load --skip_leading_rows=1 --source_format=CSV
+      ${var.project}:${google_bigquery_dataset.default.dataset_id}.${lookup(var.tables[count.index], "table_id")}
+      ${lookup(var.tables[count.index], "data")}
+    EOT
+  }
 }
-
-#resource "google_bigquery_table" "applications_offers" {
-#  dataset_id  = "${google_bigquery_dataset.default.dataset_id}"
-#  project     = var.project
-#  table_id    = "applications_offers"
-#  description = "Source: https://www.gov.uk/government/statistics/secondary-and-primary-school-application-and-offers-2019"
-#  schema      = "${file("applications_offers_schema.json")}"
-#
-#  provisioner "local-exec" {
-#    command = "bq --location=${var.location} load --skip_leading_rows=1 --source_format=CSV ${var.project}:${google_bigquery_dataset.default.dataset_id}.applications_offers ../data/2019_Apps_Offers_UD_time_series.csv"
-#  }
-#}
-#
-#resource "google_bigquery_table" "live_births" {
-#  dataset_id  = "${google_bigquery_dataset.default.dataset_id}"
-#  project     = var.project
-#  table_id    = "live_births"
-#  description = "Source: //www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/birthsummarytables"
-#  schema      = "${file("live_births_schema.json")}"
-#
-#  provisioner "local-exec" {
-#    command = "bq --location=${var.location} load --skip_leading_rows=1 --source_format=CSV ${var.project}:${google_bigquery_dataset.default.dataset_id}.live_births ../data/2017_Live_Births.csv"
-#  }
-#}
